dc2248fc065629dfee44375234b39a29d0e06e1c
Steev Klimaszewski <steev@kali.org>
2015-06-28 20:40:34 -0500

    Fix the path for the kali script

--- build.sh
+++ build.sh
@@ -4,9 +4,9 @@
 sudo apt-get install debootstrap
 
 # Fetch the latest Kali debootstrap script from git
-curl "http://git.kali.org/gitweb/?p=packages/debootstrap.git;a=blob_plain;f=scripts/kali;hb=HEAD" > kali-debootstrap &&\
-sudo debootstrap kali ./kali-root http://http.kali.org/kali ./kali-debootstrap &&\
-sudo tar -C kali-root -c . | sudo docker import - kalilinux/kali &&\
+curl "http://git.kali.org/gitweb/?p=packages/debootstrap.git;a=blob_plain;f=scripts/kali;h=50d7ef5b4e9e905cc6da8655416cdf3ef559911e;hb=refs/heads/kali/master" > kali-debootstrap &&\
+sudo debootstrap kali-rolling ./kali-root http://http.kali.org/kali ./kali-debootstrap &&\
+sudo tar -C kali-root -c . | sudo docker import - kalilinux/kali:$1 &&\
 sudo rm -rf ./kali-root &&\
 docker run -t -i kalilinux/kali cat /etc/debian_version &&\
 echo "Build OK" || echo "Build failed!"
