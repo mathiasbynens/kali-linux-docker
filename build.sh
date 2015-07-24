fb9e7ebae76a58c5e5abd4dca021d246f32e531d
Steev Klimaszewski <steev@gentoo.org>
2015-07-24 15:40:19 -0500

    Add curl to build deps in case it's not installed

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install dependencies (debbootstrap)
-sudo apt-get install debootstrap
+sudo apt-get install debootstrap curl
 
 # Fetch the latest Kali debootstrap script from git
 curl "http://git.kali.org/gitweb/?p=packages/debootstrap.git;a=blob_plain;f=scripts/kali;h=50d7ef5b4e9e905cc6da8655416cdf3ef559911e;hb=refs/heads/kali/master" > kali-debootstrap &&\
