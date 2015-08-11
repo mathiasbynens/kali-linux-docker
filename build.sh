a8bca1e5379db09056c42724c589baf43184b65a
Steev Klimaszewski <steev@kali.org>
2015-08-11 00:29:51 -0500

    Let's build for sana now, since 2.0 is releasing

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ sudo apt-get install debootstrap curl
 
 # Fetch the latest Kali debootstrap script from git
 curl "http://git.kali.org/gitweb/?p=packages/debootstrap.git;a=blob_plain;f=scripts/kali;h=50d7ef5b4e9e905cc6da8655416cdf3ef559911e;hb=refs/heads/kali/master" > kali-debootstrap &&\
-sudo debootstrap kali ./kali-root http://http.kali.org/kali ./kali-debootstrap &&\
+sudo debootstrap sana ./kali-root http://repo.kali.org/kali ./kali-debootstrap &&\
 sudo tar -C kali-root -c . | sudo docker import - kalilinux/kali &&\
 sudo rm -rf ./kali-root &&\
 docker run -t -i kalilinux/kali cat /etc/debian_version &&\
