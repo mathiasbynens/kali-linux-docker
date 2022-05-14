ed9683c5d5bad759a44b69c3b9b1b0108a49f094
Kiriii <Kiriii@protonmail.com>
2022-05-14 16:53:24 +0200

    Update branch (#8)
    * Update Dockerfile
    
    Add myself as the maintainer, and according to best practices, move the CMD line to the bottom of the file
    
    * Rebuild Sat Jun 20 10:49:32 EDT 2015
    
    * Fix the path for the kali script
    
    * Remove my local testing changes
    
    Forgot about my local changes to the build script as well, so let's remove them, so people still get a 1.x image.
    
    * Update builds
    
    * Add curl to build deps in case it's not installed
    
    * Let's build for sana now, since 2.0 is releasing
    
    * Tagging kali with current version
    
    * Move the org to be officialkali
    
    * Update the Dockerfile to point to sana
    
    * New build of Kali 2.0
    
    * Update October 30th, 2015
    
    * Update docker image to kali-rolling
    
    * Fix typos
    
    * Update Readme
    
    * Update to use the correct repo on hub
    
    * Update build
    
    * Update README.md
    
    * Update image
    
    * Update base image
    
    * Update base
    
    * Create docker-publish.yml
    
    * Create docker-image.yml
    
    * Fusionner la demande de tirage #5 de JKiriii/revert-4-JKiriii-patch-1
    
    * Delete
    
    * Circleci project setup (#6)
    
    * Add .circleci/config.yml
    
    * Add .circleci/config.yml
    
    * Ensure the `en_US.UTF-8` locale is available (#7)
    
    Co-authored-by: Mathias Bynens <mathias@qiwi.be>
    
    Co-authored-by: Steev Klimaszewski <steev@gentoo.org>
    Co-authored-by: Mati Aharoni <muts@offensive-security.com>
    Co-authored-by: Steev Klimaszewski <steev@kali.org>
    Co-authored-by: Steev Klimaszewski <threeway@gmail.com>
    Co-authored-by: silverfisk <tom@linux-konsult.com>
    Co-authored-by: Offensive Security <offensive-security@users.noreply.github.com>
    Co-authored-by: Mati Aharoni <muts@lime.local>
    Co-authored-by: Mathias Bynens <mathias@qiwi.be>

--- build.sh
+++ build.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 # Install dependencies (debbootstrap)
-sudo apt-get install debootstrap
+sudo apt-get install debootstrap curl
 
 # Fetch the latest Kali debootstrap script from git
-curl "http://git.kali.org/gitweb/?p=packages/debootstrap.git;a=blob_plain;f=scripts/kali;hb=HEAD" > kali-debootstrap &&\
-sudo debootstrap kali ./kali-root http://http.kali.org/kali ./kali-debootstrap &&\
-sudo tar -C kali-root -c . | sudo docker import - kalilinux/kali &&\
+curl "http://git.kali.org/gitweb/?p=packages/debootstrap.git;a=blob_plain;f=scripts/kali;h=50d7ef5b4e9e905cc6da8655416cdf3ef559911e;hb=refs/heads/kali/master" > kali-debootstrap &&\
+sudo debootstrap kali-rolling ./kali-root http://repo.kali.org/kali ./kali-debootstrap &&\
+sudo tar -C kali-root -c . | sudo docker import - kalilinux/kali-linux-docker &&\
 sudo rm -rf ./kali-root &&\
-docker run -t -i kalilinux/kali cat /etc/debian_version &&\
+TAG=$(sudo docker run -t -i kalilinux/kali-linux-docker awk '{print $NF}' /etc/debian_version | sed 's/\r$//' ) &&\
+echo "Tagging kali with $TAG" &&\
+sudo docker tag kalilinux/kali-linux-docker:latest kalilinux/kali-linux-docker:$TAG &&\
 echo "Build OK" || echo "Build failed!"
